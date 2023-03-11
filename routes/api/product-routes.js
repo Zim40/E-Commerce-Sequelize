81a7125e4b8c5fa6c190d35c7224ad08c48bff0a
User <michaelm810129@gmail.com>
2023-03-11 18:01:27 +1030

    Added products routes for GET/POST/DELETE

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,27 +4,64 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      include: {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      include: {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+      },
+    });
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "There has been an ERROR" });
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  // find a single product by its `id` and associated categories and tags
+  try {
+    const productById = req.params.id;
+    const product = await Product.findAll({
+      where: {
+        id: productById,
+      },
+      include: {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      include: {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+      },
+    });
+    res.json(product)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "There has been an ERROR" });
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+router.post('/', async (req, res) => {
+  try {
+    const { newProduct } = req.body;
+    const productName = await Product.create({ newProduct });
+    res.json(productName);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "There has been an ERROR" });
+  }
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
