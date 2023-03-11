b8b136e365d5a5d6e5c1a0a899f96b0ca867e627
User <michaelm810129@gmail.com>
2023-03-11 15:10:06 +1030

    added database models/catagory/product/productTag/Tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,17 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll();
+    const products = await Product.findAll();
+    res.json(categories, products);
+  } catch(error) {
+    console.error(error);
+    res.status(500).json({message: "There has been an ERROR"});
+  }
 });
 
 router.get('/:id', (req, res) => {
