9f0f6bb0cb381676ab0cc0f317f225a288ea696c
User <michaelm810129@gmail.com>
2023-03-12 14:51:15 +1030

    added Create/Post/Delete and Read routes for Tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,95 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll({
+      include: {
+        model: Product,
+      },
+    });
+    res.json(tags);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({message: 'There has been an ERROR'});
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagId = req.params.id;
+    const tag = await Tag.findAll({
+      where: {
+        id: tagId,
+      },
+      include: {
+        model: Product,
+      },
+    })
+    if (tag.length === 0) {
+      return res.status(404).send({message: 'No tag found for that Id!'});
+    }
+
+    res.json(tag);
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({message: 'There has been an ERROR!'});
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const { tag_name } = req.body;
+    const tagId = await Tag.create({ tag_name });
+    res.json(tagId);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({message: 'There has been an ERROR'});
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const { id } = req.params;
+    const { tag_name } = req.body;
+    const [updatedTag] = await Tag.update({ tag_name },
+      {
+        where: {
+          id: req.params.id
+        },
+        include: {
+          model: Product,
+          model: ProductTag,
+        },
+      },
+    );
+    res.json(updatedTag);
+  } catch (error) {
+    console.error(error)
+
+    res.status(500).send({ message: 'There has been an ERROR!' });
+
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const  { id } = req.params;
+    const tag = await Tag.findByPk(id);
+    await tag.destroy();
+    res.status(200).send({message: 'DELETED!'});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({message: 'There has been an ERROR!'});
+  }
 });
 
 module.exports = router;
